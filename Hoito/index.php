9b7273777db5d8ff127363dde0d116e750ad2564
riimala <jriimala@gmail.com>
2021-10-07 14:31:08 +0300

    Navi and  yhteydenotto forms.

--- Hoito/index.php
+++ Hoito/index.php
@@ -1,14 +1,4 @@
-<?php
-// Check if the user is logged in, otherwise redirect to login page
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
-{
-    header("location: login.php");
-    exit;
-}
-?>
-
 <!DOCTYPE html>
-
 
 <html>
 
