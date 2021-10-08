dcf5bf88c5312bddc425f9acf12fe1c489053b1b
Jouni Riimala <jriimala@gmail.com>
2021-10-08 11:34:57 +0300

    Config.php fixes within Azure details.

--- Hallinta/config.php
+++ Hallinta/config.php
@@ -8,7 +8,6 @@
 
   <?php
 
-<<<<<<< HEAD
 define('URL', 'http://localhost/Neilikka');
 define('AURL', 'https://jounir.azurewebsites.net/');
 
@@ -26,30 +25,11 @@ $un = AZURE_USERNAME;
 $ps = AZURE_PASSWORD;
 $server = AZURE_SERVER;
 $database = AZURE_DATABASE;
-=======
-  define('URL', 'http://localhost/Neilikka');
-  define('AURL', 'https://jounir.azurewebsites.net/');
 
-
-  define('LOCAL_SERVER', 'localhost');
-  define('LOCAL_DATABASE','jrla');
-
-  define('AZURE_SERVER', 'localhost:52365');
-  define('USERNAME', 'root');
-  define('PASSWORD', '');
-  define('DATABASE','neilikka');
-
-  $un = AZURE_USERNAME;
-  $ps = AZURE_PASSWORD;
-  $server = AZURE_SERVER;
-  $database = AZURE_DATABASE;
->>>>>>> 46e92c438b9a96d69ccf6d2fc3d2957139b760b7
-
-  //show all erros if any
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-
+//show all erros if any
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
   // Create connection to DB
   $link = new mysqli($server, $un, $ps, $database);
@@ -61,7 +41,6 @@ $database = AZURE_DATABASE;
   }
 
   //print "connected";
-
 
   ?>
 
